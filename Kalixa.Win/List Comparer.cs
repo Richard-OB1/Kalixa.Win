df85bb846374176a21d22b7dc62c45b279719edb
Richard-OB1 <richard.mj.obrien@gmail.com>
2016-10-27 16:52:03 +0100

    Kalixa Technical Test
    Coding Example
    Please write a short console application to intersect the below two
    lists (determine the unique set of members common to both lists) and
    output the result to the console window. DO NOT USE LINQ!
    ·         1st list {b,d,e,e,f,g,a,c,e,q,r,e,d}
    ·         2nd list {I,a,f,f,k,p,e,z,a,t}

---
+++ Kalixa.Win/List Comparer.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+
+namespace Kalixa.Win
+{
+    public class ListComparer
+    {
+        private List<char> ListOne { get; }
+        private List<char> ListTwo { get; }
+        private List<char> ListCommon { get; set; }
+
+        public ListComparer()
+        {
+            ListOne = new List<char>() { 'b', 'd', 'e', 'e', 'f', 'g', 'a', 'c', 'e', 'q', 'r', 'e', 'd' };
+            ListTwo = new List<char>() { 'I', 'a', 'f', 'f', 'k', 'p', 'e', 'z', 'a', 't' };
+            ListCommon = new List<char>();
+        }
+
+        public List<char> GetCommonList()
+        {
+            foreach (var listOneItem in ListOne)
+            {
+                foreach (var listTwoItem in ListTwo)
+                {
+                    if (listOneItem == listTwoItem) AddToCommonListIfNotEsists(listOneItem);
+                }
+            }
+            return ListCommon;
+        }
+
+        public void AddToCommonListIfNotEsists(char itemToAdd)
+        {
+            bool existInCommonList = false;
+
+            if (ListCommon.Count == 0)
+            {
+                ListCommon.Add(itemToAdd);
+            }
+            else
+            {
+                foreach (var listCommonItem in ListCommon)
+                {
+                    if (itemToAdd == listCommonItem) existInCommonList = true;
+                }
+                if (!existInCommonList) ListCommon.Add(itemToAdd);
+            }
+        }
+
+    }
+}
---
+++ Kalixa.Win/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Kalixa.Win
+{
+    /// <summary>
+    /// Tech test to find common elements in two lists using NOTHING in the Linq namespace
+    /// which would usually include IEnumerable and the Intersect extension method
+    /// Old school!
+    /// </summary>
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ListComparer listComparer = new ListComparer();
+
+            Console.WriteLine("Kalixa Technical Test");
+
+            Console.WriteLine($"Characters in common in List One and List Two: {CharListToString(listComparer.GetCommonList())}");
+
+            Console.WriteLine("Press enter to close...");
+
+            Console.ReadLine();
+        }
+
+        public static string CharListToString(List<char> charList)
+        {
+            return string.Concat(charList.ToArray());
+        }
+
+    }
+}
